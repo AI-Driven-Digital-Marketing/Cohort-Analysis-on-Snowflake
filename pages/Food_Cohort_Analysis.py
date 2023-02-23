3be7b20f8684fe1f03e07ec26bc771aea988ce4c
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-02-23 13:35:38 -0500

    Update Food_Cohort_Analysis.py

--- pages/Food_Cohort_Analysis.py
+++ pages/Food_Cohort_Analysis.py
@@ -88,11 +88,9 @@ def load_data():
     food_df.columns = [x.lower() for x in food_df.columns]
     # Write our codes here -- INFO Teams!
     # Process data
-   
-#     food_df["orderdate"] = pd.to_datetime(food_df["orderdate"], format="'%m/%d/%Y")
     food_df["orderdate"] = pd.to_datetime(food_df["orderdate"]).dt.date
-#     food_df['orderdate'] = pd.to_date(food_df['orderdate'], format = '%Y/%m/%d')   
-#     #food_df['pickupdate'] = pd.to_date(food_df['pickupdate'], format ='%Y/%m/%d')
+    food_df["pickupdate"] = pd.to_datetime(food_df["pickupdate"]).dt.date
+
     
     return food_df
 
