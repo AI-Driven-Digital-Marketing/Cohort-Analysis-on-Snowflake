a9bc883a2ee56869b181492e1e143ef3ba7547fe
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-02-24 15:33:54 -0500

    Update Food_Cohort_Analysis.py

--- pages/Food_Cohort_Analysis.py
+++ pages/Food_Cohort_Analysis.py
@@ -178,4 +178,14 @@ fig.add_heatmap(
         # colorscale="Aggrnyl",
         colorscale="Bluyl",
     )
-     
+fig.update_layout(title_text="Monthly cohorts showing retention rates", title_x=0.2)
+    fig.layout.xaxis.title = "Cohort Group"
+    fig.layout.yaxis.title = "Cohort Period"
+    fig["layout"]["title"]["font"] = dict(size=25)
+    fig.layout.width = 750
+    fig.layout.height = 750
+    fig.layout.xaxis.tickvals = retention.columns
+    fig.layout.yaxis.tickvals = retention.index
+    fig.layout.plot_bgcolor = "#efefef"  # Set the background color to white
+    fig.layout.margin.b = 100
+    fig
