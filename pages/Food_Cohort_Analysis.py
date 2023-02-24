78e3438f23a01ddd1c2bf506b26c6976026c7998
Smart Husky <64514218+DDuan-zw@users.noreply.github.com>
2023-02-24 15:54:42 -0500

    update

--- pages/Food_Cohort_Analysis.py
+++ pages/Food_Cohort_Analysis.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Cohort Analysis on the Food dataset", page_icon="
 
 st.image(
     "Food.jpg",
-    width=400,
+    use_column_width = True,
 )
 
 st.title("Cohort Analysis → `Food` dataset")
@@ -99,44 +99,80 @@ def load_data():
     # Assigning a minimum InvoiceMonth value to the dataset
     food_df['CohortMonth'] = grouping.transform('min')
     
-    def get_date_int(df, column):
-        year = df[column].dt.year
-        month = df[column].dt.month
-        day = df[column].dt.day
-        return year, month, day
-    # Getting the integers for date parts from the `InvoiceDay` column
-    transcation_year, transaction_month, _ = get_date_int(food_df, 'TransactionMonth')
-
-    # Getting the integers for date parts from the `CohortDay` column
-    cohort_year, cohort_month, _ = get_date_int(food_df, 'CohortMonth')
-    #  Get the  difference in years
-    years_diff = transcation_year - cohort_year
-
-    # Calculate difference in months
-    months_diff = transaction_month - cohort_month
-    food_df['CohortIndex'] = years_diff * 12 + months_diff  + 1 
-    # Counting daily active user from each chort
-    grouping = food_df.groupby(['CohortMonth', 'CohortIndex'])
 
 
 
-    # Counting number of unique customer Id's falling in each group of CohortMonth and CohortIndex
-    cohort_data = grouping['USERID'].apply(pd.Series.nunique)
-    cohort_data = cohort_data.reset_index()
+    return food_df
 
 
-     # Assigning column names to the dataframe created above
-    cohort_counts = cohort_data.pivot(index='CohortMonth',
-                                     columns ='CohortIndex',
-                                     values = 'USERID')
-    cohort_sizes = cohort_counts.iloc[:,0]
-    retention = cohort_counts.divide(cohort_sizes, axis=0)
-    #retention.round(3)*100
+food_df = load_data()
+
+with st.expander("Show the `Food` dataframe"):
+    st.write(food_df)
+
+@st.cache_data
+def get_minmaxCharges():
+    return food_df.TOTALCHARGES.max(), food_df.TOTALCHARGES.min()
+
+max_charge, min_charge = get_minmaxCharges()
+_, col1, _ = st.columns([0.2, 1, 0.2])
+
+with col1:
+    TotalCharges_slider = st.slider(
+            "Total Charges (in $)",  0.0,  max_charge-0.01, 
+        )    
+
+food_df = food_df[food_df["TOTALCHARGES"] > TotalCharges_slider]
+def get_date_int(df, column):
+    year = df[column].dt.year
+    month = df[column].dt.month
+    day = df[column].dt.day
+    return year, month, day
+# Getting the integers for date parts from the `InvoiceDay` column
+transcation_year, transaction_month, _ = get_date_int(food_df, 'TransactionMonth')
+
+# Getting the integers for date parts from the `CohortDay` column
+cohort_year, cohort_month, _ = get_date_int(food_df, 'CohortMonth')
+#  Get the  difference in years
+years_diff = transcation_year - cohort_year
+
+# Calculate difference in months
+months_diff = transaction_month - cohort_month
+food_df['CohortIndex'] = years_diff * 12 + months_diff  + 1 
+# Counting daily active user from each chort
+grouping = food_df.groupby(['CohortMonth', 'CohortIndex'])
 
 
-    return food_df,retention
+
+# Counting number of unique customer Id's falling in each group of CohortMonth and CohortIndex
+cohort_data = grouping['USERID'].apply(pd.Series.nunique)
+cohort_data = cohort_data.reset_index()
 
 
-food_df,retention = load_data()
-food_df
-retention
+ # Assigning column names to the dataframe created above
+cohort_counts = cohort_data.pivot(index='CohortMonth',
+                                 columns ='CohortIndex',
+                                 values = 'USERID')
+cohort_sizes = cohort_counts.iloc[:,0]
+retention = cohort_counts.divide(cohort_sizes, axis=0)
+retention.index = retention.index.strftime('%Y-%m')
+
+fig = go.Figure()
+fig.add_heatmap(
+        x=retention.columns,
+        y=retention.index,
+        z=retention,
+        colorscale="Viridis"
+    )
+
+fig.update_layout(title_text="Monthly cohorts showing retention rates", title_x=0.2)
+fig.layout.xaxis.title = "Cohort Group"
+fig.layout.yaxis.title = "Cohort Period"
+fig["layout"]["title"]["font"] = dict(size=25)
+fig.layout.width = 750
+fig.layout.height = 750
+fig.layout.xaxis.tickvals = retention.columns
+fig.layout.yaxis.tickvals = retention.index
+fig.layout.plot_bgcolor = "#efefef"  # Set the background color to white
+fig.layout.margin.b = 100
+fig
