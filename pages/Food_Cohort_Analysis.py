c06c6db39c4459b186889f2cd0343a822a2d82d4
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-02-24 21:20:41 -0500

    Update Food_Cohort_Analysis.py

--- pages/Food_Cohort_Analysis.py
+++ pages/Food_Cohort_Analysis.py
@@ -157,7 +157,7 @@ retention = cohort_counts.divide(cohort_sizes, axis=0)
 retention.index = retention.index.strftime('%Y-%m')
 
 
-percent_retention = (retention * 100)
+percent_retention = (retention * 100).round(1)
 
 fig = go.Figure()
 fig.add_heatmap(
