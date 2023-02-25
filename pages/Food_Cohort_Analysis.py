409fd36913b4cf12a16a0ecc2f386fd915e09da2
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-02-24 21:15:26 -0500

    Update Food_Cohort_Analysis.py

--- pages/Food_Cohort_Analysis.py
+++ pages/Food_Cohort_Analysis.py
@@ -156,6 +156,7 @@ cohort_sizes = cohort_counts.iloc[:,0]
 retention = cohort_counts.divide(cohort_sizes, axis=0)
 retention.index = retention.index.strftime('%Y-%m')
 
+
 percent_retention = (retention * 100).round(1)
 
 fig = go.Figure()
@@ -165,7 +166,7 @@ fig.add_heatmap(
         z=retention,
         text=percent_retention,
         hoverinfo='text', 
-        colorscale="Viridis", 
+        colorscale="Viridis"
     )
 
 fig.update_layout(title_text="Monthly cohorts showing retention rates", title_x=0.2)
